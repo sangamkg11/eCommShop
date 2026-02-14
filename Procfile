d3dcc33fbceb92913736854dad517da3c2cd7ab3
sangamkg11 <sangamkg11@gmail.com>
2026-02-14 16:40:39 +0530

    final...

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eCommShop.wsgi:application
--- eCommShop/settings.py
+++ eCommShop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True,cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -61,6 +61,9 @@ MIDDLEWARE = [
 
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 
